38f408fa2151b18903e12055d6f083ab6a4e482b
gaozb <2287158979@qq.com>
2024-02-20 09:32:52 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/es/test/ESTest_Client.java
@@ -0,0 +1,11 @@
+package com.atguigu.es.test;
+
+/**
+ * @author gaozb
+ * @date 2024/2/19 10:47
+ */
+public class ESTest_Client {
+    public static void main(String[] args) {
+        //创建ES客户端
+    }
+}
